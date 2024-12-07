7f3374d28277e78ef9b171d7c752b18a159c64a9
Avi Gupta <savim2020@gmail.com>
2024-12-07 11:09:37 -0500

    Changed canMake to work; shoutout to u/cranebirdidk on reddit!

--- y2024/day07/main.go
+++ y2024/day07/main.go
@@ -38,8 +38,8 @@ func concat(i1, i2 int) int {
 	return i1*n + i2
 }
 
-// original solution, did not work for part 2 for some reason lol
-// works fine for part 1 :)
+// original solution, updated to work for both parts!
+// huge shoutout to u/cranebirdidk on Reddit for finding the mistake
 func (e *Equation) canMake(base int) bool {
 	perms := int(math.Pow(float64(base), float64(
 		len(e.operands)-1,
@@ -48,7 +48,7 @@ func (e *Equation) canMake(base int) bool {
 outer:
 	for p := range perms {
 		perm, n := p, e.operands[0]
-		for _, v := range e.operands[1:] {
+		for i, v := range e.operands[1:] {
 			switch operators[perm%base] {
 			case "+":
 				n += v
@@ -58,7 +58,10 @@ outer:
 				n = concat(n, v)
 			}
 
-			if n == e.num {
+			// mistake was here.
+			// originally, I was checking to ensure that n == e.num.
+			// however, I should have checked to see if I was checking the last operand.
+			if n == e.num && i == len(e.operands[1:])-1 {
 				return true
 			}
 			if n > e.num {
@@ -68,36 +71,6 @@ outer:
 		}
 	}
 	return false
-}
-
-// from https://github.com/mnml/aoc/blob/main/2024/07/1.go. Idk how this works for part 2
-// but not canMake. :(
-func (e *Equation) isPossible(base int) bool {
-	var f func(int, int) bool
-	f = func(idx, total int) bool {
-		if total > e.num {
-			return false
-		}
-		if idx == len(e.operands) {
-			return total == e.num
-		}
-
-		c := false
-		v := e.operands[idx]
-		for i := range base {
-			switch operators[i] {
-			case "+":
-				c = c || f(idx+1, total+v)
-			case "*":
-				c = c || f(idx+1, total*v)
-			case "||":
-				c = c || f(idx+1, concat(total, v))
-			}
-		}
-		return c
-	}
-
-	return f(1, e.operands[0])
 }
 
 func part1(input string) string {
@@ -115,7 +88,7 @@ func part2(input string) string {
 	eqs := parseInput(input)
 	sum := 0
 	for _, eq := range eqs {
-		if eq.isPossible(3) {
+		if eq.canMake(3) {
 			sum += eq.num
 		}
 	}
